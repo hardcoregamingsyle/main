024cf84e8ef3f795de214d6843239b864272d9a7
hardcoregamingsyle <hardcorgamingstyle@gmail.com>
2026-08-16 20:05:08 +0530

    Coder: Creating server.js - Express server with helmet
    [FILE CREATED: server.js]
    
    [CMD: cat server.js]...

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
