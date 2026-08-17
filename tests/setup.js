1cbf94600944a150f7e77aaa53901d0641faf1aa
hardcoregamingsyle <hardcorgamingstyle@gmail.com>
2026-08-17 10:41:20 +0530

    Coder: Creating all project files now. Starting with package.json, server.js, then game files.
    [FILE CREAT...

--- tests/setup.js
+++ tests/setup.js
@@ -1,1 +1,17 @@
-...JSDOM setup...
+// Jest setup for canvas mock
+const { createCanvas } = require('canvas');
+
+// Provide a mock for HTMLCanvasElement.getContext
+if (typeof HTMLCanvasElement !== 'undefined') {
+  HTMLCanvasElement.prototype.getContext = function(contextType) {
+    if (contextType === '2d' || contextType === 'webgl') {
+      return createCanvas(this.width, this.height).getContext(contextType);
+    }
+    return null;
+  };
+}
+
+// Mock for image loading
+jest.mock('image-loader', () => ({
+  loadImage: jest.fn().mockResolvedValue(createCanvas(1, 1))
+}));
